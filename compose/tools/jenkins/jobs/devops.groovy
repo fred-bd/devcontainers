b17e85efaa11416215ae6b8073c2d5053965e2d4
Frederico Bede <bede.apps@gmail.com>
2025-10-24 00:57:33 +0000

    [BAI-62] - Adding more stages in flux pipelines

--- compose/tools/jenkins/jobs/devops.groovy
+++ compose/tools/jenkins/jobs/devops.groovy
@@ -30,6 +30,9 @@ pipelineJob("devops/configure-flux") {
     stringParam('VaultAddr', 'http://localhost:8200', 'The address to the vault')
     stringParam('KubeFilePath', 'cluster-hosts/note', 'The path to vault secret containing kubeconfig file')
     stringParam('KubeFileSecret', 'note-kubeconfig', 'The key name of the secret containing kubeconfig file')
+    booleanParam('CleanFluxResources', true, 'Remove all resources from the cluster including flux')
+    booleanParam('ConfigureSharding', true, 'Use additional shard controllers')
+    stringParam('KubeAuthNs', 'cert-manager,grafana,kiali', 'Namespaces to copy kubernetes auth secret to use in vault')
   }
 
   displayName("Configure flux resources")
